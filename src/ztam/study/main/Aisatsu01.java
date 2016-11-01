51c3011d3e38c5a15d4f745f2c43c75215043b61
mitsutaka_matsuoka <mitsutaka_matsuoka@s09501>
2016-11-01 16:52:42 +0900

    commit

---
+++ src/ztam/study/main/Aisatsu01.java
@@ -0,0 +1,10 @@
+package ztam.study.main;
+
+public class Aisatsu01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("matzは\"Java\"を勉強しています。");
+		}
+
+}
