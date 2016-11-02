3e4e78650ceffd78212c8fedb5070ca14a5bf4f4
mitsutaka_matsuoka <mitsutaka_matsuoka@s09501>
2016-11-02 18:32:54 +0900

    テスト２回目

--- src/ztam/study/main/Talk01.java
+++ src/ztam/study/main/Talk01.java
@@ -1,13 +1,15 @@
 package ztam.study.main;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Talk01 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int age;
-		System.out.println("Ｙｏｕ、名前は？");
+		System.out.println("ユー、名前は？");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 				try {
 					String line = reader.readLine();
@@ -17,6 +19,11 @@ public class Talk01 {
 					age = Integer.parseInt(line);
 					System.out.println(age + "歳か、なるほどな。");
 					System.out.println("ってことは 100年後には" + (age + 100) + "歳だな。");
+				} catch (IOException e) {
+					System.out.println(e);
+				} catch (NumberFormatException e) {
+					System.out.println("年齢間違ってる！");
+				}
 	}
 
 }
