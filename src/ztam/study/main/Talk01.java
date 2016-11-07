68dd9a10d0a51d5327303f19830e9acaf4274fbb
mitsutaka_matsuoka <mitsutaka_matsuoka@s09501>
2016-11-07 09:12:26 +0900

    TEST

--- src/ztam/study/main/Talk01.java
+++ src/ztam/study/main/Talk01.java
@@ -10,12 +10,12 @@ public class Talk01 {
 		// TODO 自動生成されたメソッド・スタブ　　　
 		int age;
 		System.out.println("ユー、名前は？");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 				try {
-					String line = reader.readLine();
+					String line = input.readLine();
 					System.out.println("ＨＥＬＬＯ " + line);
 					System.out.println(line + "は何歳なんだい？");
-					line = reader.readLine();
+					line = input.readLine();
 					age = Integer.parseInt(line);
 					System.out.println(age + "歳か、なるほどな。");
 					System.out.println("ってことは 100年後には" + (age + 100) + "歳だな。");
