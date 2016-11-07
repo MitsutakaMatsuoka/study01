fd0f3fc92e953174aa8a654ad5b384a12a11a558
mitsutaka_matsuoka <mitsutaka_matsuoka@s09501>
2016-11-07 13:53:05 +0900

    TEST4

---
+++ src/ztam/study/main/Calc02.java
@@ -0,0 +1,14 @@
+package ztam.study.main;
+
+public class Calc02 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int mm = 60;
+		int hh = 60;
+		int day = 24;
+		long hikari = 300000L * mm * hh * day;
+		System.out.println("光は日で　" + hikari  + "ｋｍ進みます。");
+	}
+
+}
---
+++ src/ztam/study/main/Calc03.java
@@ -0,0 +1,59 @@
+package ztam.study.main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calc03 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		BufferedReader input1;
+		BufferedReader input2;
+		String name1;
+		String name2;
+		int age1;
+		int age2;
+		System.out.println("キミの名前は？");
+		System.out.println("");
+		input1 = new BufferedReader(new InputStreamReader(System.in));
+		input2 = new BufferedReader(new InputStreamReader(System.in));
+				try {
+					String line = input1.readLine();
+					System.out.println("ＨＥＬＬＯ " + line);
+					System.out.println("");
+					name1 = line;
+					System.out.println(name1 + "は何歳なんだい？");
+					System.out.println("");
+					line = input1.readLine();
+					age1 = Integer.parseInt(line);
+					System.out.println(age1 + "歳か、なるほどな。");
+					System.out.println("");
+					System.out.println("ってことは 100年後には" + (age1 + 100) + "歳だな。");
+					System.out.println("");
+					System.out.println("");
+					System.out.println("そっちのキミ、名前は？");
+					System.out.println("");
+					line = input2.readLine();
+					System.out.println("ＨＥＬＬＯ " + line);
+					System.out.println("");
+					name2 = line;
+					System.out.println(name2 + "は何歳なんだい？");
+					System.out.println("");
+					line = input2.readLine();
+					age2 = Integer.parseInt(line);
+					System.out.println(age2 + "歳か、なるほどな。");
+					System.out.println("");
+					System.out.println("ってことは 100年後には" + (age2 + 100) + "歳だな。");
+					System.out.println("");
+					System.out.println("");
+					int heikin = (age1 + age2) / 2;
+					System.out.println(name1 + " と " + name2 + " の年齢平均は" + heikin + "歳だな。");
+				} catch (IOException e) {
+					System.out.println(e);
+				} catch (NumberFormatException e) {
+					System.out.println("年齢間違ってる！");
+				}
+	}
+
+}
