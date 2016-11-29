f4875fb355eea125e4c8cdd51fd56109ab2ef0f4
Mitsutaka_Matsuoka <Mitsutaka_Matsuoka@S15956.sisco.saison.co.jp>
2016-11-29 11:36:38 +0900

    TEST2

--- src/ztam/study/main/ShowWhiteFile2.java
+++ src/ztam/study/main/ShowWhiteFile2.java
@@ -10,9 +10,10 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class ShowWhiteFile2 {
@@ -21,12 +22,13 @@ public class ShowWhiteFile2 {
 		try{
 			  File r_file = new File("C:\\SISCOTGIF05.csv");
 			  FileInputStream input = new FileInputStream(r_file);
-			  InputStreamReader stream = new InputStreamReader(input,"SJIS");
-			  BufferedReader b_reader = new BufferedReader(stream);
+			  InputStreamReader i_stream = new InputStreamReader(input,"SJIS");
+			  BufferedReader b_reader = new BufferedReader(i_stream);
 
 			  File w_file = new File("C:\\SISCOTGIF05w.csv");
-			  FileWriter output = new FileWriter(w_file);
-			  BufferedWriter b_writer = new BufferedWriter(output);
+			  FileOutputStream output = new FileOutputStream(w_file);
+			  OutputStreamWriter o_stream = new OutputStreamWriter(output,"SJIS");
+			  BufferedWriter b_writer = new BufferedWriter(o_stream);
 			  PrintWriter writer = new PrintWriter(b_writer,true);
 
 			  String line;
@@ -35,11 +37,12 @@ public class ShowWhiteFile2 {
 			    writer.println(line);
 			  }
 			  input.close();
-			  stream.close();
+			  i_stream.close();
 			  b_reader.close();
 			  output.close();
+			  o_stream.close();
 			  b_writer.close();
-			  writer.close();
+
 			}catch(FileNotFoundException e){
 			  System.out.println(e);
 			}catch(IOException e){
